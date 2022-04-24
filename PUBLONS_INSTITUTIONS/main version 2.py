aca80f9cf13f7d83905de610760290e627cdb541
ItaloLamaG <italo.lama@alumnos.upla.cl>
2022-04-24 11:00:39 -0400

    Añadida versión final código main
    Obtiene y guarda en archivo .csv todos los institutos listados en Publons.

--- PUBLONS_INSTITUTIONS/main version 2.py
+++ PUBLONS_INSTITUTIONS/main version 2.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[3]:
 
 
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+import time
 
 lista_instituciones = []
 
 #Página base
-from requests_html import AsyncHTMLSession
-asession = AsyncHTMLSession()
-r = await asession.get("https://publons.com/institution/?order_by=num_researchers")
-await r.html.arender(sleep=5) # Este llamado ejecuta los JS que se encuentran en la página
+options = Options()
+options.add_argument('--headless')
+driver = webdriver.Firefox(options=options, executable_path='geckodriver')
+driver.get("https://publons.com/institution/?order_by=num_researchers")
 
-soup = BeautifulSoup(r.html.raw_html, 'html.parser') #parse (analizador)
+SCROLL_PAUSE_TIME = 1.5
+# Get scroll height
+last_height = driver.execute_script("return document.body.scrollHeight")
+print('Scrolling hasta el final de la pagina...', end='')
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        break
+    last_height = new_height
+    print('.', end='')
+
+print('\n')
+
+html = driver.page_source
+soup = BeautifulSoup(html, 'html.parser')
+
 
 #Bloque donde se encuentra cada institución
 inst = soup.findAll('div', attrs={"class":'flex-row'})
